8e95cd8be9aacdd1f8966dccb761b2c3d3dd1946
Evanoob <60706676+Evanoob@users.noreply.github.com>
2020-10-16 22:22:09 +0200

    Test sans error

---
+++ handler.js
@@ -0,0 +1,14 @@
+import axios from "axios";
+
+export function apiHandler() {
+    // if (!process.env.VUE_APP_BACKEND_URL)
+    // throw new Error("Fournir URL de base pour effectuer app AJAX");
+
+    const instance = axios.create({
+        // 1 méthode d'axios
+        baseURL: process.env.VUE_APP_BACKEND_URL, // pour normaliser la connection à l'API backend
+    });
+
+    return instance;
+}
+// 
